c008cba5f12eb9f71221f7253583068097cebc38
Anup <themightysapien@gmail.com>
2023-05-27 00:43:31 +0545

    Added Modal wc Added Uploader component Styling in progress

--- src/components/TsMediaLibrary.ce.vue
+++ src/components/TsMediaLibrary.ce.vue
@@ -1,13 +1,167 @@
 <template>
-    <div>
-            Hello world
-    </div>
+	<div>
+		<section
+			v-if="opened"
+			:class="modalClass"
+			@click="_onModalBackgroundClick"
+			@transitionend="onAnimationEnd"
+			id="tml-backgroundmodal"
+		>
+			<div
+				class="tml-content"
+				@click="onContentClick"
+			>
+				<span
+					class="tml-modal-close"
+					@click="onClose"
+				></span>
+				<article class="">
+					<Uploader />
+				</article>
+			</div>
+		</section>
+		<span @click="open">
+			<slot> {{ label }} </slot>
+		</span>
+	</div>
 </template>
 <script>
-export default {
-    
-}
+	import Uploader from "./Uploader.vue";
+
+	export default {
+		components: { Uploader },
+		props: {
+			blocking: { default: false },
+			label: { default: "Upload" },
+		},
+		data() {
+			return {
+				opened: false,
+				_toChange: false,
+			};
+		},
+		methods: {
+			open() {
+				this.opened = true;
+				this._toChange = true;
+				setTimeout(() => {
+					this._toChange = false;
+				}, 50);
+			},
+			_onModalBackgroundClick(e) {
+				if (
+					!this.blocking &&
+					e.target &&
+					e.target.getAttribute("id") === "tml-backgroundmodal"
+				) {
+					this.close();
+					this.$emit("tml-modal-background-closed", {
+						bubbles: true,
+						composed: true,
+						detail: this,
+					});
+				}
+			},
+
+			close() {
+				this.opened = false;
+				this._toChange = true;
+				this.$emit("tml-modal-closed", {
+					bubbles: true,
+					composed: true,
+					detail: this,
+				});
+			},
+
+			onAnimationEnd() {
+				this._toChange = false;
+			},
+		},
+		computed: {
+			modalClass() {
+				if (!this.opened && !this._toChange) {
+					return "transparent";
+				}
+				if (this.opened && this._toChange) {
+					return "transparent opened";
+				}
+				if (this.opened && !this._toChange) {
+					return "opaque";
+				}
+				if (!this.opened && this._toChange) {
+					return "transparent opened";
+				}
+			},
+		},
+	};
 </script>
-<style>
-    
+<style scoped>
+	* {
+		box-sizing: border-box;
+	}
+	:host {
+		--tml-close-icon-template-color: var(--tml-modal-close-icon-color, #888);
+	}
+	section {
+		position: fixed;
+		left: 0;
+		top: 0;
+		height: 100vh;
+		width: 100vw;
+		display: none;
+		transition: opacity var(--tml-modal-animation-duration, 0.3s) ease-in;
+		-webkit-transition: opacity var(--tml-modal-animation-duration, 0.3s) ease-in;
+		align-items: center;
+		justify-content: center;
+		z-index: var(--tml-modal-z-index, 100);
+		background-color: var(--tml-modal-background-color, rgba(30, 30, 30, 0.8));
+	}
+	.tml-content {
+		display: block;
+		position: relative;
+		z-index: var(--tml-modal-content-z-index, 101);
+		width: var(--tml-modal-width, 280px);
+		min-width: var(--tml-modal-min-width, 250px);
+		max-width: var(--tml-modal-max-width, 100vw);
+		height: var(--tml-modal-height, auto);
+		min-height: var(--tml-modal-min-height, auto);
+		max-height: var(--tml-modal-max-height, 100vh);
+		background-color: var(--tml-modal-content-background-color, #fff);
+		box-shadow: var(--tml-modal-content-shadow-displacement, 6px)
+			var(--tml-modal-content-shadow-displacement, 6px)
+			var(--tml-modal-content-shadow-blur, 16px)
+			var(--tml-modal-content-shadow-color, #000);
+		border-radius: var(--tml-modal-border-radius, 15px);
+		padding: var(--tml-modal-content-padding, 1em) 0
+			var(--tml-modal-content-padding, 1em) var(--tml-modal-content-padding, 1em);
+	}
+	article {
+		overflow: auto;
+		max-height: 100%;
+		height: 100%;
+		padding-right: var(--tml-modal-content-padding, 1em);
+	}
+	.transparent {
+		opacity: 0;
+	}
+	.opaque {
+		opacity: 1;
+		display: flex !important;
+	}
+	.opened {
+		display: flex !important;
+	}
+	.tml-modal-close {
+		position: absolute;
+		display: inline-block;
+		top: var(--tml-modal-close-icon-top, 5px);
+		right: var(--tml-modal-close-icon-right, 18px);
+		z-index: var(--tml-modal-close-icon-z-index, 1002);
+		width: var(--tml-modal-close-icon-size, 24px);
+		height: var(--tml-modal-close-icon-size, 24px);
+		cursor: var(--tml-modal-close-icon-cursor, pointer);
+	}
+	.contentIconSeparation {
+		padding-top: var(--tml-modal-extra-top-separation-when-icon, 10px);
+	}
 </style>
---
+++ src/components/Uploader.vue
@@ -0,0 +1,186 @@
+<template>
+  <div>
+    <input type="file" ref="auto_file_input" style="display: none" @change="fileChanged" :multiple="multiple" />
+
+    <span :class="inputContainerClass" @click="$refs['auto_file_input'].click()">
+      <slot v-if="(fileList && fileList.length == 0) || isBtnVisible">
+        <button type="button" class="btn btn-primary">
+          Upload
+        </button>
+      </slot>
+    </span>
+
+    <!-- <ProgressBar v-if="showProgress && progressBar.visible" :value="progressBar.completed">
+      <strong>{{ progressBar.completed }}</strong>
+    </ProgressBar> -->
+
+    <template v-if="preview && fileList.length">
+      <span v-for="(file, $index) in fileList" :key="'file_url_' + file.id + '_' + $index"
+        class="preview flex items-center">
+        <slot name="preview" v-if="preview" v-bind:file="file">
+          <img :src="file.thumb_url" class="rounded-circle" alt="..." @click.prevent="$emit('imageClicked', file)"
+            :style="{ width: previewSize, height: previewSize }" />
+        </slot>
+        <span @click.prevent="removeItem(file, $index)">
+          <slot name="remove">
+            <a class="preview_remove">
+              <span class="badge badge-pill">X</span>
+            </a>
+          </slot>
+        </span>
+      </span>
+    </template>
+  </div>
+</template>
+
+<script>
+// import ProgressBar from "primevue/progressbar";
+// import * as mediaAPI from "@app/services/mediaAPI";
+// import { buildFormData } from "@app/helpers";
+export default {
+  components: {  },
+  props: {
+    autoHide: { default: false },
+    multiple: { default: 0 },
+    preview: { default: true },
+    info: { default: true },
+    previewSize: { default: "80px" },
+    inputContainerClass: { default: "" },
+    previewContainerClass: { default: "" },
+    removeConfirm: { default: true },
+    allowFiles: { default: 0 },
+    removeConfirmText: {
+      default: "Are you sure you want to remove this file?",
+    },
+    resetFilesOnUpload: { default: false },
+    showProgress: { default: true },
+    name: { default: "files[]" },
+    params: {
+      default: function () {
+        return {};
+      },
+    },
+    files: {
+      default: function () {
+        return [];
+      },
+    },
+    maxLength: { default: 0 },
+  },
+  emits: ["changed"],
+  data() {
+    return {
+      //   name: "files[]",
+      data: null,
+      fileList: [],
+      progressBar: {
+        visible: false,
+        total: 100,
+        completed: 0,
+        completed_byte: 0,
+      },
+    };
+  },
+  methods: {
+    fileChanged($event) {
+      let formData = new FormData();
+      //   console.log(this.$refs["auto_file_input"].files);
+      if (this.multiple) {
+        Array.from(Array(this.$refs["auto_file_input"].files.length).keys()).map(
+          (x) => {
+            formData.append(
+              this.name,
+              this.$refs["auto_file_input"].files[x],
+              this.$refs["auto_file_input"].files[x].name
+            );
+          }
+        );
+      } else {
+        formData.append(
+          this.name,
+          this.$refs["auto_file_input"].files[0],
+          this.$refs["auto_file_input"].files[0].name
+        );
+      }
+      formData.append("allowFiles", this.allowFiles);
+
+      this.upload(formData);
+    },
+    upload(data) {
+      data = buildFormData(data, this.params);
+      this.progressBar.visible = true;
+      mediaAPI
+        .upload(data, {
+          onUploadProgress: (progressEvent) => {
+            var percentCompleted = Math.round(
+              (progressEvent.loaded * 100) / progressEvent.total
+            );
+            this.progressBar.completed = percentCompleted;
+            // console.log(progressEvent.loaded, this.progressBar.completed);
+          },
+        })
+        .then((resp) => {
+          if (!resp["success"]) {
+            return;
+          }
+          if (this.multiple) {
+            this.fileList = this.fileList.concat(resp.items);
+          } else {
+            this.fileList = resp.items;
+          }
+          this.progressBar.visible = false;
+          this.output();
+          this.reset();
+        })
+        .catch((e) => {
+          this.progressBar.visible = false;
+        });
+    },
+    output() {
+      // console.log(this.getFileData());
+      this.$emit("changed", this.getFileData());
+    },
+    reset() {
+      this.$refs["auto_file_input"].value = null;
+      if (this.resetFilesOnUpload) {
+        this.fileList = [];
+      }
+    },
+    removeItem(file, index) {
+      // console.log(index);
+      if (this.removeConfirm && !confirm(this.removeConfirmText)) {
+        return;
+      }
+      mediaAPI.remove(file.id).then((resp) => {
+        this.fileList.splice(index, 1);
+        this.output();
+      });
+    },
+    clear() {
+      this.reset();
+      this.fileList = [];
+    },
+    getFileData() {
+      return this.fileList;
+    },
+  },
+  mounted() {
+    //   console.log(this.files);
+    this.fileList = this.files.filter((item) => item);
+  },
+  computed: {
+    isBtnVisible() {
+      if (
+        this.multiple &&
+        this.maxLength > 0 &&
+        this.fileList.length >= this.maxLength
+      ) {
+        return false;
+      }
+      return !this.autoHide;
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>
