5554b274bf64f56f3660bb7068a81fb2cfc879f8
Anup <themightysapien@gmail.com>
2023-05-29 01:07:48 +0545

    helpers

---
+++ src/helpers/index.js
@@ -0,0 +1,12 @@
+export function humanFileSize(size) {
+	if (size == 0) {
+		return 0;
+	}
+    
+	var i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
+	return (
+		(size / Math.pow(1024, i)).toFixed(2) * 1 +
+		" " +
+		["B", "kB", "MB", "GB", "TB"][i]
+	);
+}
