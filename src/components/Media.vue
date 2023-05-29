c097c72bb0d1faa7a9c316a029c7c4a585cc4c53
Anup <themightysapien@gmail.com>
2023-05-30 01:39:49 +0545

    added reusable media render

---
+++ src/components/Media.vue
@@ -0,0 +1,20 @@
+<template>
+	<div v-if="media">
+		<img
+			:src="media.thumb_url"
+			v-bind="$attrs"
+		/>
+	</div>
+</template>
+<script>
+	export default {
+		props: {
+			media: { default: null },
+		},
+		computed: {
+			isImage() {
+				return this.media && this.media.mime_type.indexOf("image") >= 0;
+			},
+		},
+	};
+</script>
