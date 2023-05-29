ae2599caef48090e257791a00e406024db2a0d01
Anup <themightysapien@gmail.com>
2023-05-30 01:38:49 +0545

    added selected previews wip

---
+++ src/components/SelectedPreview.vue
@@ -0,0 +1,23 @@
+<template>
+	<div
+		class="tml-selections-preview"
+		v-if="collection && collection.length"
+	>
+		<div class="tml-selection-item"
+			v-for="(media, i) of collection"
+			:key="i"
+		>
+			<Media :media="media" />
+		</div>
+	</div>
+</template>
+<script>
+	import Media from "./Media.vue";
+	export default {
+		components: { Media },
+		props: {
+			collection: { default: () => [] },
+		},
+	};
+</script>
+<style lang=""></style>
