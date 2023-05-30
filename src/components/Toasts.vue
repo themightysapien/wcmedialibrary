4c846bc00fca6d13cf40dfccb9e686f621281829
Anup <themightysapien@gmail.com>
2023-05-30 20:44:05 +0545

    toast feature added

---
+++ src/components/Toasts.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="tml-toasts">
+		<!-- <div class="toast error active">HELLO</div> -->
+		<div
+			v-for="(toast, $t) of toasts"
+			:key="'toast_'.$t"
+			class="toast"
+			:class="[toast.type, { active: toast.active }]"
+		>
+			{{ toast.message }}
+		</div>
+	</div>
+</template>
+<script setup>
+	import useToasts from "../composables/useToast";
+	import { inject } from "vue";
+	const uid = inject("uid");
+	const { state: toasts } = useToasts(uid);
+</script>
+<style></style>
---
+++ src/composables/useToast.ts
@@ -0,0 +1,57 @@
+import { ref, computed, reactive } from "vue";
+
+interface Toast {
+	message: string;
+	type: string;
+  active: boolean;
+}
+const toastStore = reactive<{ [key: string]: Toast[] }>({
+	default: [],
+});
+
+export default function useToasts(key = "default", timer = 5000) {
+	const state = toastStore[key];
+	// const state = reactive({
+	//   status: val,
+	//   contextStatus: {}
+	// })
+
+	const success = (message: string) => {
+		state.push({
+			message,
+			type: "success",
+      active: false
+		});
+		clear(state.length - 1);
+	};
+	const error = (message: string) => {
+		state.push({
+			message,
+			type: "error",
+      active: false
+		});
+		clear(state.length - 1);
+	};
+
+	const clear = (index: number) => {
+    setTimeout(() => {
+      state[index].active = true
+    }, 500);
+		setTimeout(() => {
+			state.splice(index, 1);
+		}, timer);
+	};
+
+
+
+	return {
+		initStore: (key: string) => {
+			if (!toastStore[key]) {
+				toastStore[key] = [];
+			}
+		},
+    state,
+		success,
+    error
+	};
+}
